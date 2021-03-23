0084b7ad1941e044a07ea26fb6b081211503845a
Fengli2017 <35957466+Fengli2017@users.noreply.github.com>
2021-03-23 16:32:15 +0800

    Create build.groovy

---
+++ src/org/devops/build.groovy
@@ -0,0 +1,11 @@
+package org.devops
+
+//build type
+
+def Build(buildType,buildShell){
+    def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
+    println("Selected Build Type is ${buildType}")
+    buildHome- = tool buildTools[buildType]
+  
+    batch """${buildHome}/bin${buildType} &{buildShell}"""
+}
