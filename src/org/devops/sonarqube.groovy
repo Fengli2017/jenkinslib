5d8ddda0979b6460809a548c6965cacfabdf33ff
Fengli2017 <35957466+Fengli2017@users.noreply.github.com>
2021-04-09 14:49:30 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -5,7 +5,9 @@ def SonarScan(projectName,projectDesc,projectPath){
     //def scannerHome = "C:/DevOps/Sonar-Scanner/bin/sonar-scanner"
     def sonarServer = "http://localhost:9000"
     def sonarDate = bat returnStdout: true, script: 'echo %DATE:~0,4%%DATE:~5,2%%DATE:~8,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%'
+    println("${sonarDate}")
     sonarDate = sonarDate - "\n"
+    println("${sonarDate}")
         
     bat """
         C:/DevOps/Sonar-Scanner/bin/sonar-scanner -Dsonar.host.url=${sonarServer} -Dsonar.projectKey=${projectName} -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.login=admin -Dsonar.password=123456 -Dsonar.ws.timeout=30 -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.baidu.com -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports
