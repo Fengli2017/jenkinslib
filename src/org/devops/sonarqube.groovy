b58cedfca074b97b04458bc85f9dab1ba35e6674
Fengli2017 <35957466+Fengli2017@users.noreply.github.com>
2021-04-09 16:22:35 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -5,7 +5,7 @@ def SonarScan(projectName,projectDesc,projectPath){
     //def scannerHome = "C:/DevOps/Sonar-Scanner/bin/sonar-scanner"
     def sonarServer = "http://localhost:9000"
     def sonarDate = bat returnStdout: true, script: """echo %DATE:~0,4%%DATE:~5,2%%DATE:~8,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%"""
-    sonarDate = sonarDate[-14..-1]
+    sonarDate = sonarDate[-16..-3]
     println("${sonarDate}")
     //sonarDate = sonarDate - "\n"
     /*    
